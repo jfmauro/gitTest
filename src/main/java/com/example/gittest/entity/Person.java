d16444214c66ec33936ce7b0739f4b36277ec7dc
MAURO JEAN-FRANCOIS <jeanfrancois.mauro@minfin.fed.be>
2021-12-23 17:02:26 +0100

    add status to Person

--- src/main/java/com/example/gittest/entity/Person.java
+++ src/main/java/com/example/gittest/entity/Person.java
@@ -1,7 +1,10 @@
 package com.example.gittest.entity;
 
+import com.example.gittest.enume.PersonStatus;
+
 public class Person {
     private Long id;
     private String username;
     private String password;
+    private PersonStatus status;
 }
