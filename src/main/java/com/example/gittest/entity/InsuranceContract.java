9d3e2566d6b1ab9aa12f0801db8534c1502ed5c1
MAURO JEAN-FRANCOIS <jeanfrancois.mauro@minfin.fed.be>
2021-12-23 17:24:34 +0100

    add InsuranceContract

---
+++ src/main/java/com/example/gittest/entity/InsuranceContract.java
@@ -0,0 +1,8 @@
+package com.example.gittest.entity;
+
+public class InsuranceContract {
+    private Long id;
+    private String name;
+    private String amount;
+    private Person p;
+}
--- src/main/java/com/example/gittest/entity/Person.java
+++ src/main/java/com/example/gittest/entity/Person.java
@@ -2,9 +2,12 @@ package com.example.gittest.entity;
 
 import com.example.gittest.enume.PersonStatus;
 
+import java.util.List;
+
 public class Person {
     private Long id;
     private String username;
     private String password;
     private PersonStatus status;
+    private List<InsuranceContract> insuranceContracts;
 }
